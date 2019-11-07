508c1efcc6935964a8d0d90be391ff1509ad053e
yi <mavisluan83@gmail.com>
2019-11-06 21:16:28 -0800

    Controlled Input -- two ways of submitting input data

--- src/components/ControlledInput.js
+++ src/components/ControlledInput.js
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
 
 const ControlledInput = () => {
     const [name, setName] = useState({
@@ -11,8 +13,14 @@ const ControlledInput = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
         // sort an array of objects by property value
-        const newContact = contact.concat({name, phone}).sort((a, b) => (a.name.lastName > b.name.lastName) ? 1: -1);
+        const newContact = contact.concat({name, phone}).sort((a, b) => (a.name.lastName > b.name.lastName) ? 1 : -1);
         setContact(newContact);
+        // clear input
+        setName({
+            firstName: '',
+            lastName: ''
+        });
+        setPhone('')
     };
 
     /*  When modifying one property's value of an object,
@@ -20,15 +28,29 @@ const ControlledInput = () => {
         name = {firstName: '', lastName: ''}
         onChange={(e) => setName({...name, firstName: e.target.value })}/>
     */
+
+    /*
+        Ways to submit a form
+        1) Use <form>
+            -> pass submit function to form -->    <form onSubmit={handleSubmit}>
+            -> submit button (type: submit)
+            -> handleSubmit function (e.preventDefault() -> to stop the form submitting the input data)
+            -> setState functions
+
+        2) Use <input> only
+          -> pass handleSubmit to submit button -->  <button onClick={handleSubmit}>Submit</button>
+          -> handleSubmit function doesn't need to have e.preventDefault()
+    */
     return (
-        <div>
+        <div className="container mt-5 justify-content-around align-items-center">
             <form onSubmit={handleSubmit}>
                 <input type="text" placeholder={'FirstName'} value={name.firstName}
-                       onChange={(e) => setName({...name, firstName: e.target.value })}/>
+                       onChange={(e) => setName({...name, firstName: e.target.value})}/>
                 <input type="text" placeholder={'LastName'} value={name.lastName}
                        onChange={(e) => setName({...name, lastName: e.target.value})}/>
                 <input type="text" placeholder={'Phone'} value={phone} onChange={(e) => setPhone(e.target.value)}/>
-                <input type="submit"/>
+                <input type="submit" className="btn-primary"/>
+                {/*<Button variant={"primary"} onClick={handleSubmit}>Submit</Button>*/}
             </form>
             <div>
                 <ol>
