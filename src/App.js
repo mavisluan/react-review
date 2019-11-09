35bf9feba1fcc24130d9a6a56bf2535a26a4e66e
yi <mavisluan83@gmail.com>
2019-11-08 19:10:59 -0800

    Updated README

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import './App.css';
-import ControlledInput from './components/ControlledInput';
-import { RefInput, RefInputStateless } from './components/UncontrolledInput';
+import ControlledInput from './components/inputs/ControlledInput';
+import {
+  RefInput,
+  RefInputStateless,
+} from './components/inputs/UncontrolledInput';
 
 function App() {
   return (
