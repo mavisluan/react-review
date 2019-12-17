34ee8ad3a04d053f4cc0093a94c047a854939457
yi <mavisluan83@gmail.com>
2019-12-17 11:28:47 -0800

    Add reset button and reset function

--- src/components/class/Child.js
+++ src/components/class/Child.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Child({ onAction }) {
+function Child({ onAction, resetCounter }) {
   return (
-    <button
-      type="button"
-      onClick={onAction}
-      className="btn-primary rounded mt-3"
-    >
-      Click Me!
-    </button>
+    <div>
+      <button
+        type="button"
+        onClick={onAction}
+        className="btn-primary rounded mt-3"
+      >
+        Click Me!
+      </button>
+      <button
+        className="btn-danger rounded ml-4"
+        type="button"
+        onClick={() => resetCounter()}
+      >
+        Reset
+      </button>
+    </div>
   );
 }
 
 Child.propTypes = {
   onAction: PropTypes.func.isRequired,
+  resetCounter: PropTypes.func.isRequired,
 };
 
 export default Child;
--- src/components/class/CoutingParent.js
+++ src/components/class/CoutingParent.js
@@ -14,6 +14,7 @@ export default class CoutingParent extends Component {
     // Bind the event handler function, so that its 'this'
     // binding isn't lost when it gets passed to the button
     this.handleAction = this.handleAction.bind(this);
+    this.resetCounter = this.resetCounter.bind(this);
   }
 
   handleAction(action) {
@@ -25,12 +26,19 @@ export default class CoutingParent extends Component {
     });
   }
 
+  resetCounter(action) {
+    const { actionCount } = this.state;
+    this.setState({
+      actionCount: 0,
+    });
+  }
+
   render() {
     const { actionCount } = this.state;
 
     return (
       <div>
-        <Child onAction={this.handleAction} />
+        <Child onAction={this.handleAction} resetCounter={this.resetCounter} />
         <div className="mt-3">Clicked {actionCount} times</div>
       </div>
     );
