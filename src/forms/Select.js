e2ac901ad126315aa63addaf160fdded0cbfa380
yi <mavisluan83@gmail.com>
2019-12-23 14:27:40 -0800

    update Select with dynamic options

--- src/forms/Select.js
+++ src/forms/Select.js
@@ -2,19 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Form } from 'react-bootstrap';
 
-const Select = ({ handleInput }) => (
+const Select = ({ handleInput, options }) => (
   <Form.Group className="d-inline-flex">
     <Form.Control as="select" name="ageRange" onChange={handleInput}>
       <option>Choose your age range</option>
-      <option value="18-25">18-25</option>
-      <option value="26-59">26-59</option>
-      <option value="60 or older">60 or older</option>
+      {options.map(option => (
+        <option value={option}>{option}</option>
+      ))}
     </Form.Control>
   </Form.Group>
 );
 
 Select.propTypes = {
   handleInput: PropTypes.func.isRequired,
+  options: PropTypes.array.isRequired,
 };
 
 export default Select;
