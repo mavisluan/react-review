685e82c650ded052d6ce2588f8d290be10edd892
yi <mavisluan83@gmail.com>
2019-12-27 17:05:00 -0500

    Created fake-data.json and fetch data with axios

--- src/forms/OrderPizza.js
+++ src/forms/OrderPizza.js
@@ -1,19 +1,43 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import axios from 'axios';
 import SingleInput from './SingleInput';
 import CheckboxOrRadioGroup from './CheckboxOrRadioGroup';
 import Select from './Select';
 
-const sizeOptions = ['small', 'medium', 'large'];
+// const sizeOptions = ['small', 'medium', 'large'];
 const OrderPizza = () => {
-  const initialState = {
+  const [pizza, setPizza] = useState({
     userName: '',
-    size: ['large'],
-    topping: 'Pepperoni',
+    sizeOptions: [],
+    selectedSize: [],
+    toppingOptions: [],
+    selectedTopping: '',
     glutenFree: false,
-    notes: 'extra crispy',
-  };
-  const [pizza, setPizza] = useState(initialState);
+    notes: '',
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data } = await axios.get('./pizza-static-data.json'); // static data is in pubic folder
+        setPizza(data);
+        console.log('data', data);
+      } catch (e) {
+        console.log('err', e);
+      }
+    };
+    fetchData();
+  }, []);
+
+  // const initialState = {
+  //   userName: '',
+  //   size: ['large'],
+  //   topping: 'Pepperoni',
+  //   glutenFree: false,
+  //   notes: 'extra crispy',
+  // };
+  // const [pizza, setPizza] = useState(initialState);
   const handleInput = e => {
     const { name, value } = e.target;
     setPizza({
@@ -22,7 +46,15 @@ const OrderPizza = () => {
     });
   };
 
-  const { userName, size, topping, glutenFree, notes } = pizza;
+  const {
+    userName,
+    sizeOptions,
+    selectedSize,
+    toppingOptions,
+    selectedTopping,
+    glutenFree,
+    notes,
+  } = pizza;
 
   const sizeSelection = ({ selectedOptions, item, setName }) => {
     if (!selectedOptions.includes(item))
@@ -43,8 +75,8 @@ const OrderPizza = () => {
         options={sizeOptions}
         title="size"
         type="radio"
-        selectedOptions={size}
-        setName="size"
+        selectedOptions={selectedSize}
+        setName="selectedSize"
         controlFunction={sizeSelection}
       />
     </Container>
