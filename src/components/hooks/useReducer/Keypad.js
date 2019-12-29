1150195b38a11feb659403caac82e058d4cbe07e
yi <mavisluan83@gmail.com>
2019-12-29 17:31:03 -0500

    Add generate code randomly

--- src/components/hooks/useReducer/Keypad.js
+++ src/components/hooks/useReducer/Keypad.js
@@ -1,66 +1,73 @@
+/* eslint-disable no-param-reassign */
 import React, { useReducer } from 'react';
-import { Container, Button, ButtonGroup, Row, Col } from 'react-bootstrap';
+import { Container, Button, ButtonGroup } from 'react-bootstrap';
 
-const unlockOrder = [3, 5, 6, 2, 4, 1];
+const buttons = [1, 2, 3, 4, 5, 6];
+const generateUnlockCode = () => {
+  const code = new Array(6).fill(0);
+  code.forEach((num, i) => {
+    const randomNum = Math.floor(Math.random() * 6 + 1);
+    code[i] = num + randomNum;
+  });
+  console.log('secret code', code);
+  return code;
+};
+
+const unlockOrder = generateUnlockCode();
+
 const initialState = {
-  order: unlockOrder,
+  show: false,
+  position: 0,
+  code: unlockOrder,
   status: ['_', '_', '_', '_', '_', '_'],
 };
 
 const reducer = (state, action) => {
-  const first = state.order[0];
-  const index = unlockOrder.indexOf(action);
-  const newStatus = [...state.status];
-  newStatus[index] = '*';
-
-  if (first === action) {
+  const { position, code, status, show } = state;
+  if (code[position] === action) {
+    const newStatus = [...status];
+    newStatus[position] = '*';
     return {
       ...state,
-      order: state.order.filter(num => num !== action),
+      position: position + 1,
       status: newStatus,
     };
   }
+
   return initialState;
 };
+
 const Keypad = () => {
   const [state, dispatch] = useReducer(reducer, initialState);
   console.log('state', state);
-  const { order, status } = state;
+  const { position, status } = state;
   return (
     <Container className="d-flex flex-column col-4">
       <div className="py-3 text-center">
-        <h3>{order.length === 0 ? 'Unlocked' : 'Enter 6 digit code'}</h3>
+        <h3>Enter 6 digit code</h3>
+        <h3 className="text-success">{position === 6 && 'Unlocked'}</h3>
         <div className="display-4">
           {status.map((s, index) => (
             <span key={index}> {s}</span>
           ))}
         </div>
       </div>
-      <ButtonGroup>
-        <Button onClick={() => dispatch(1)} className="border border-white">
-          1
-        </Button>
-        <Button onClick={() => dispatch(2)} className="border border-white">
-          2
-        </Button>
-        <Button onClick={() => dispatch(3)} className="border border-white">
-          3
-        </Button>
+      <ButtonGroup className="d-flex flex-wrap">
+        {buttons.map((val, index) => (
+          <Button
+            onClick={() => dispatch(val)}
+            className="border col-4"
+            key={index}
+          >
+            {val}
+          </Button>
+        ))}
       </ButtonGroup>
-      <ButtonGroup>
-        <Button onClick={() => dispatch(4)} className="border border-white">
-          4
-        </Button>
-        <Button onClick={() => dispatch(5)} className="border border-white">
-          5
-        </Button>
-        <Button onClick={() => dispatch(6)} className="border border-white">
-          6
-        </Button>
-      </ButtonGroup>
+      <Button variant="danger" onClick={() => dispatch('reset')}>
+        Clear
+      </Button>
     </Container>
   );
 };
 
 export default Keypad;
-// Todo: 1. Generate code randomly  2. Show effect when entering the right digit or wrong
