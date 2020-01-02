3c2c2456620ff37d17b047f633b2989afaf686e7
yi <mavisluan83@gmail.com>
2020-01-02 12:50:18 -0500

    Refactor Navbar

--- src/components/hooks/contextAPI/Navbar.js
+++ src/components/hooks/contextAPI/Navbar.js
@@ -1,28 +1,24 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
-import { UsersContext } from './UsersStore';
+import { WithUsersContext } from './UsersProvider';
 
-const Navbar = () => (
-  <UsersContext.Consumer>
-    {users => (
-      <Row className="bg-warning p-3">
-        {console.log('users', users)}
-        <Col className="d-flex flex-wrap">
-          {users.map(user => (
-            // <img
-            //   src={user.picture.thumbnail}
-            //   width="75em"
-            //   alt="avatar"
-            //   key={user.id}
-            // />
-            <span className="mr-2">{user.name.first}</span>
-          ))}
-        </Col>
-      </Row>
-    )}
-  </UsersContext.Consumer>
+const Navbar = ({ usersProvider }) => (
+  <Row className="bg-warning p-3">
+    {console.log('usersProvider', usersProvider)}
+    <Col className="d-flex flex-wrap">
+      {usersProvider.map(user => (
+        <span className="mr-2" key={user.id}>
+          {user.name.first}
+        </span>
+      ))}
+    </Col>
+  </Row>
 );
-// Navbar.propTypes = {};
+Navbar.propTypes = {
+  usersProvider: PropTypes.array.isRequired,
+  // Provider receives value of users array
+  // usersProvider= value = users array
+};
 
-export default Navbar;
+export default WithUsersContext(Navbar);
